73285e6d96aa1467093559516054289f7b27384a
Riciokzz <113688473+Riciokzz@users.noreply.github.com>
2023-02-15 14:50:17 +0200

    Coordinates finder added to Work folder
    Created work folder
    Added coordinates finder file

---
+++ projects/Work/koordinates.py
@@ -0,0 +1,94 @@
+import os
+from tkinter import messagebox
+import tkinter as tk
+from tkinter import filedialog
+
+def browse_folder():
+    folder_path = filedialog.askdirectory()
+    folder_label.config(text=folder_path)
+    return folder_path
+
+def browse_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+    file_label.config(text=file_path)
+    return file_path
+
+
+def submit():
+    x = float(x_entry.get())
+    y = float(y_entry.get())
+    r = float(r_entry.get())
+    folder_path = folder_label.cget("text")
+    file_path = file_label.cget("text")
+
+    all_txt = []
+    file_count = 0
+    coordinate_count = 0
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".txt"):
+            file_count += 1
+            with open(os.path.join(folder_path, filename), 'r') as file:
+                for line in file:
+                    values = line.strip().split(',')
+                    if len(values) >= 4:
+                        x_value = float(values[1])
+                        y_value = float(values[2])
+                        if ((x_value - x) ** 2 + (y_value - y) ** 2) ** 0.5 <= r:
+                            all_txt.append(line)
+                            coordinate_count += 1
+
+    with open(file_path, 'w') as file:
+        file.writelines(all_txt)
+
+    tk.messagebox.showinfo("Atlikta",
+                           f"Atlikta be klaidų. Nuskaityti {file_count} .txt failai. Pridėtos {coordinate_count} koordinatės į {file_path}.")
+
+def close_win():
+   root.destroy()
+
+root = tk.Tk()
+root.title("Taškų paieška v0.2")
+
+folder_path = ""
+
+folder_browse_button = tk.Button(root, text="Pasirinkti failus", command=browse_folder)
+folder_browse_button.grid(row=0, column=0)
+
+folder_label = tk.Label(root, text="Nepasirinkta papkė")
+folder_label.grid(row=0, column=1)
+
+file_browse_button = tk.Button(root, text="Kur išsaugoti", command=browse_file)
+file_browse_button.grid(row=1, column=0)
+
+file_label = tk.Label(root, text="Nepasirinktas failas")
+file_label.grid(row=1, column=1)
+
+x_label = tk.Label(root, text="X Koordinatė:")
+x_label.grid(row=2, column=0)
+
+x_entry = tk.Entry(root)
+x_entry.grid(row=2, column=1)
+
+y_label = tk.Label(root, text="Y Koordinatė:")
+y_label.grid(row=3, column=0)
+
+
+y_entry = tk.Entry(root)
+y_entry.grid(row=3, column=1)
+y_entry.insert(0, "0")
+
+
+r_label = tk.Label(root, text="Atstumas m:")
+r_label.grid(row=4, column=0)
+
+r_entry = tk.Entry(root)
+r_entry.grid(row=4, column=1)
+
+submit_button = tk.Button(root, text="Ieškoti", command=submit)
+submit_button.grid(row=5, column=0, pady=0)
+
+close_button = tk.Button(root, text="Uždaryti", command=close_win)
+close_button.grid(row=5, column=1, pady=0)
+
+
+root.mainloop()
